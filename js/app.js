3189b1f71a30858db66bef717f014549d09b6aab
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-25 19:17:48 -0400

    find all other same combat styles, disable them/if target is un-checked, enable all other same combat styles

--- js/app.js
+++ js/app.js
@@ -9,12 +9,30 @@ form.addEventListener('change', (e) => {
   //  - Select all checkboxes from the DOM that share the same combat style as the triggering checkbox.
   if (e.target.type === 'checkbox') {
     const targetedCheck = e.target
+    //targetedCheck is the targeted element
+    //dataset gives access to all 'data-' attributes in camel case
+    //data-combat-style becomes dataset.combatStyle
+    const combatStyle = targetedCheck.dataset.combatStyle
+    //grab checkboxes, filter only combatStyles that match
+    const matches = document.querySelectorAll(
+      `input[type="checkbox"][data-combat-style="${combatStyle}"]`
+    )
+
+    //3a. Next, loop over each checkbox with the matching combat style:
+    for (let i = 0; i < matches.length; i++) {
+      const checkbox = matches[i]
+      //  - If: the checkbox being looped over is NOT the one that triggered the event,
+      //    and the triggering checkbox is checked, then:
+      //    - Disable the checkbox currently being looped over.
+      //make sure we're not modifying what was just checked
+      if (checkbox !== targetedCheck && targetedCheck.checked) {
+        checkbox.disabled = true
+        //  - Else:
+      } else {
+        //    - Do not change its state.
+        //if triggering checkbox is being unchecked, re-enable the others
+        checkbox.disabled = false
+      }
+    }
   }
 })
-
-//3a. Next, loop over each checkbox with the matching combat style:
-//  - If: the checkbox being looped over is NOT the one that triggered the event,
-//    and the triggering checkbox is checked, then:
-//    - Disable the checkbox currently being looped over.
-//  - Else:
-//    - Do not change its state.
