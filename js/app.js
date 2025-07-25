984757432330809027eabcd112163858969a7f30
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-25 18:57:34 -0400

    add event listener for form, target checkbox that triggers change

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 //1. Target the form element in the DOM.
-  
-//2. Attach an event listener to the form element to listen for any change events.
+const form = document.querySelector('form')
 
-//3. Inside the event listener:
-//  - Target the checkbox that triggered the change event.
-//  - Get the combat style of the triggering checkbox from its data-combat-style attribute.
-//  - Select all checkboxes from the DOM that share the same combat style as the triggering checkbox.
-  
+//2. Attach an event listener to the form element to listen for any change events.
+form.addEventListener('change', (e) => {
+  //3. Inside the event listener:
+  //  - Target the checkbox that triggered the change event.
+  //  - Get the combat style of the triggering checkbox from its data-combat-style attribute.
+  //  - Select all checkboxes from the DOM that share the same combat style as the triggering checkbox.
+  if (e.target.type === 'checkbox') {
+    const targetedCheck = e.target
+  }
+})
+
 //3a. Next, loop over each checkbox with the matching combat style:
 //  - If: the checkbox being looped over is NOT the one that triggered the event,
 //    and the triggering checkbox is checked, then:
